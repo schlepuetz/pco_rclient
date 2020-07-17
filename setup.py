f55794915a25de7ac5a62e3c5ac1fbfa160eafdd
Leonardo Hax Damiani <leonardo.hax@psi.ch>
2020-07-17 19:41:13 +0200

    Bump conda package version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'Readme.md')).read()
 
 setup(name='pco_rclient',
-      version='0.1.0',
+      version='0.3.0',
       description="Rest client script for the pco writer.",
       long_description=README,
       author='Paul Scherrer Institute (PSI)',
