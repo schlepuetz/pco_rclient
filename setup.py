381861091642068fa74719e293d503eba5e7f869
Leonardo Hax Damiani <leonardo.hax@psi.ch>
2020-09-15 15:10:50 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pco_rclient',
-      version='0.4.3',
+      version='0.4.4',
       description="Rest client script for the pco writer.",
       author='Paul Scherrer Institute (PSI)',
       maintainer="Paul Scherrer Institute",
